80a30340ef7eec1cbd3fd7f3af9e128626680252
Sashks <alex.butschie@gmail.com>
2019-02-05 17:32:43 +0100

    text2speech and different canvases for mobile and desktop

--- sketch.js
+++ sketch.js
@@ -1,6 +1,8 @@
 let mobilenet;
 let video;
 let label = '';
+let temp;
+let counter = 0;
 
 function modelReady() {
   console.log('Model READY!');
@@ -18,26 +20,66 @@ function gotResults(error, results) {
   }
 }
 
-function setup() {
-  createCanvas(350, 500);
-  video = createCapture({
-    audio: false,
-    video: {
-      facingMode: {
-        exact: "environment"
-      }
-    }
-  });
-  background(0);
-  mobilenet = ml5.imageClassifier('MobileNet', video, modelReady);
+function isMobileDevice() {
+    return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
+};
+
+
+if(!isMobileDevice) {
+	function setup() {
+		createCanvas(350, 450);  
+		video = createCapture({
+			video: {
+				facingMode: {
+					exact: "user"
+				}
+			}
+		});
+		video.hide();
+		background(0);
+		mobilenet = ml5.imageClassifier('MobileNet', video, modelReady);
+	}
+
+	function draw() {
+		background(0);
+		image(video, 0, 0);
+		fill(255);
+		textSize(14);    
+		if(temp != label) {
+			var speech = new SpeechSynthesisUtterance(label);
+			speech.rate = 3;
+			speechSynthesis.speak(speech); 
+			console.log("speach ready mobile");
+		}
+		temp = label;
+		text(label, 10, height - 20);
+		text("mobile", 15, height - 30);
+	}
+} else {
+	function setup() {
+		createCanvas(600, 500);  
+		video = createCapture(VIDEO);
+		video.hide();
+		background(0);
+		mobilenet = ml5.imageClassifier('MobileNet', video, modelReady);
+	}
+
+	function draw() {
+		background(0);
+		image(video, 0, 0);
+		fill(255);
+		textSize(32);    
+		if(temp != label) {
+			var speech = new SpeechSynthesisUtterance(label);
+			speech.rate = 3;
+			speechSynthesis.speak(speech); 
+			console.log("speach ready");
+		}
+		temp = label;
+		text(label, 10, height - 20);
+	}
 }
 
-function draw() {
-  background(0);
-  image(video, 0, 0);
-  fill(255);
-  textSize(32);  
-  var msg = new SpeechSynthesisUtterance(label);
-  window.speechSynthesis.speak(msg);  
-  text(label, 10, height - 20);
-}
+
+
+
